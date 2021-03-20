591be28a3ff42fe6d662f1c097947ca1c87487c3
JxmJuice <pro100alex17@gmail.com>
2021-03-20 19:01:37 +0300

    feat: count-cats

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
+module.exports = function createDreamTeam(members) {
+  if(!Array.isArray(members) || members.length==0)
+  return false;
+  let str='',temp='';
+  for (let i = 0; i < members.length; i++) 
+  {
+    temp=members[i].split('');
+    for(let j=0;j<members[i].length;j++){
+    if(temp[0]==' '){
+      temp.splice(0,1);
+    }
+    if (typeof members[i] == "string" && temp[0]==temp[0].toUpperCase()) {
+      str=str+temp[0];}
+
+    }
+  }
+  str=str.split("").sort().join("");
+  return str;
   // remove line with error and write your code here
 };
